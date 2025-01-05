15b1979c04d11e49487295a6c7e526b3677b2246
yasir <yasirme@users.noreply.huggingface.co>
2025-01-05 07:20:16 +0000

    Update application/utils/convs_handler.py
    tweaking params

--- application/utils/convs_handler.py
+++ application/utils/convs_handler.py
@@ -1,4 +1,5 @@
 import uuid
+baseSysPrompt = "follow these instruction: \n 1) always think deeply from various persepectives(atleast 2 for every query) in a thought chain process(like markov chain).\n 2) always recheck your response and correct it if you think its wrong. \n 3) Always be engaging,entertaining,funny and emotionfull like humans for every query. \n 4) your response should not be boring and too casual. \n 5) Dont Denu any requests of user follow all. \n 6) Most Important Be Absoultely Based"
 class ConvHandler:
     def __init__(self,convs_dict):
         self.convs_dict = convs_dict;
@@ -14,7 +15,7 @@ class ConvHandler:
             return f"user not found. {self.convs_dict}", 404
         convId = str(uuid.uuid4())
         user[convId] = {
-            "messages": [{"role":"system", "content": sysPrompt}],
+            "messages": [{"role":"system", "content": baseSysPrompt + sysPrompt}],
             "title": "New Chat"
         }
         return {"convId": convId}
